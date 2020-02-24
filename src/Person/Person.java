c0403a481ed3d86fadacb6fba6609ed9b5a02b35
iamkushmittal <mittalkush1999>
2020-02-24 21:55:26 +0530

    Created Person Class in Person.java declaring variables

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -10,7 +10,101 @@ public class Person implements Comparable<Person> {
     private String email;
     private long phoneNumber;
 
+    public Person() {
+    }
 
+    public Person(String fName, String lName, String email, long phoneNumber, List<Long> list) {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.list = list;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        list.add(phoneNumber);
+    }
+
+    public List<Long> getList() {
+        return list;
+    }
+
+    //This method addString() add the list of phone number in StringBuilder in particular format.
+    private StringBuilder addString() {
+        StringBuilder sb = new StringBuilder();
+        int k = 0;
+        for (long i : list) {
+            sb.append(i);
+            k++;
+            sb.append((k <= list.size() - 1 ? "," : ""));
+            sb.append(" ");
+        }
+        return sb;
+    }
+
+    @Override
+    public String toString() {
+        return "-------- * -------- * -------- * --------\n" +
+                "First Name: " + fName + "\n" +
+                "Last Name: " + lName + "\n" +
+                (list.size() == 1 ? "Contact Number: " : "Contact Number(s):") + addString() + "\n" +
+                "Email Address: " + email + "\n" +
+                "-------- * -------- * -------- * --------";
+    }
+
+    //This method compare the fName of two persons.
+    @Override
+    public int compareTo(Person person) {
+        return fName.compareTo(person.fName);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Person guest = (Person) obj;
+        return fName.compareToIgnoreCase(guest.fName) >= 1;
+    }
+}
 
 
 
