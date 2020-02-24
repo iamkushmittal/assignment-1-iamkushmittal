25505d3668fa8118716e75016c6d5f1d69e6ad4d
iamkushmittal <mittalkush1999>
2020-02-24 21:51:59 +0530

    Created Person Class in Person.java declaring variables

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -1,5 +1,18 @@
 package Person;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Person {
+public class Person implements Comparable<Person> {
+    //This list contains the phone numbers.
+    List<Long> list = new ArrayList<>();
+    private String fName;
+    private String lName;
+    private String email;
+    private long phoneNumber;
+
+
+
+
+
 
 }
